9a0592752fa4995ed779ce3a8a5e641c63144a97
Brandon Larkin <bllarkin@gmail.com>
2020-05-19 08:16:36 -0700

    Add z plugin support

--- installzsh.sh
+++ installzsh.sh
@@ -1,6 +1,6 @@
-
-
 # Install zsh autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 # Install zsh syntax highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+# Install z plugin
+curl https://github.com/rupa/z/blob/master/z.sh -o ~/z.sh
