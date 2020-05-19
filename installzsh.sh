d77410e31200ba7427e3e4c808f2ce70bcbbdc7d
Brandon Larkin <bllarkin@gmail.com>
2020-05-19 07:56:33 -0700

    Add installzsh.sh to the repo

---
+++ installzsh.sh
@@ -0,0 +1,6 @@
+
+
+# Install zsh autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+# Install zsh syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
